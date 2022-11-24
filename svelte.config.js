20a3c5173afbb6601e20105a2a688b3f6e5e63a1
taoguangc <taoguangc@gmail.com>
2022-11-24 16:53:51 +0800

    change output folder to public

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,8 @@ const config = {
   kit: {
     adapter: adapter({
       // default options are shown
-      pages: "build",
-      assets: "build",
+      pages: "public",
+      assets: "public",
       fallback: null,
     })
   },
