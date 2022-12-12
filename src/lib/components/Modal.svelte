ed5638e58a7d5ca400cd918353e899e57749f226
taoguangc <taoguangc@gmail.com>
2022-12-12 11:10:36 +0800

    updated modal's width

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -4,10 +4,10 @@ export let video
 </script>
 
 {#if $isModalOpen}
-<div class="w-auto fixed z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-10 bg-white">
-  <button class="w-8 h-8 text-4xl text-primary absolute right-2 top-0" on:click={() => isModalOpen.set(false)}>&times;</button>
+<div class="w-full xl:w-auto fixed z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md xl:p-10 bg-white">
+  <button class="w-8 h-8 text-4xl text-primary absolute z-50 right-2 top-0" on:click={() => isModalOpen.set(false)}>&times;</button>
   <slot>
-    <video width="800" height="540" controls>
+    <video width="1280" height="720" controls>
       <source src={video} type="video/mp4">
       <track default kind="captions">
     </video>
