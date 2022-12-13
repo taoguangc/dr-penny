f373490b7ec84e4e939050805c8b9ac215cffffe
taoguangc <taoguangc@gmail.com>
2022-12-13 14:19:54 +0800

    视频自动播放

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -7,7 +7,7 @@ export let video
 <div class="w-full xl:w-auto fixed z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md xl:p-10 bg-white">
   <button class="w-8 h-8 text-4xl text-primary absolute z-50 right-2 top-0" on:click={() => isModalOpen.set(false)}>&times;</button>
   <slot>
-    <video width="1280" height="720" controls>
+    <video width="1280" height="720" controls autoplay>
       <source src={video} type="video/mp4">
       <track default kind="captions">
     </video>
