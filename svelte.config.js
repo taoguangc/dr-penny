e99e431d39a36fbb7a7675b1451198a5c330b536
taoguangc <taoguangc@gmail.com>
2023-04-12 17:06:36 +0800

    SEO titles

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,20 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
       // default options are shown
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-      // pages: 'public',
-      // assets: 'public',
-      // fallback: null,
+      // routes: {
+      //   include: ['/*'],
+      //   exclude: ['<all>']
+      // }
+      pages: 'public',
+      assets: 'public',
+      fallback: null,
     })
   },
+}
 
-};
-
-export default config;
+export default config
