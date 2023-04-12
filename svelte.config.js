e6f955cc23f78ba92d24812d7d40d27667297189
taoguangc <taoguangc@gmail.com>
2023-04-12 14:36:48 +0800

    change to @sveltejs/adapter-cloudflare and update devDependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
       // default options are shown
-      pages: 'public',
-      assets: 'public',
-      fallback: null,
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+      // pages: 'public',
+      // assets: 'public',
+      // fallback: null,
     })
   },
 
