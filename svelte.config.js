79f9222c7c8cbe28d31ab7f4e240c728e43ca189
taoguangc <taoguangc@gmail.com>
2023-06-26 11:02:22 +0800

    update ssg to adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static"
+import adapter from "@sveltejs/adapter-cloudflare"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
       // default options are shown
-      // routes: {
-      //   include: ['/*'],
-      //   exclude: ['<all>']
-      // }
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      },
       pages: 'public',
       assets: 'public',
       fallback: null,
