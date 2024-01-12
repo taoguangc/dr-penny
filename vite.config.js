c400804b96ce82b2f26c09281767a8eba53a53d2
taoguangc <taoguangc@gmail.com>
2024-01-12 17:12:48 +0800

    update to sveltekit last versin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { defineConfig } from 'vite';
 
-const config = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+	plugins: [enhancedImages(), sveltekit()]
+});
